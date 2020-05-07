e841f7cf8687eff46f2924dc97189ce6311311db
Frank Luongo <fluongo@oreilly.com>
2020-05-07 09:39:40 -0400

    CH1 | Add Additional Tests

--- ch1/test-driven-fizzbuzz/index.test.js
+++ ch1/test-driven-fizzbuzz/index.test.js
@@ -10,4 +10,14 @@ describe("fizzbuzz()", () => {
     expect(fizzBuzz(1)).toBe("1");
     expect(fizzBuzz(22)).toBe("22");
   });
+
+  it('returns the "Fizz" for multiples of 3', () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+    expect(fizzBuzz(6)).toBe("Fizz");
+  });
+
+  it('returns "Buzz" for multiples of 5', () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
 });
