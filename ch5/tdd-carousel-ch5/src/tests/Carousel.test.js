49ac8819ed77ed4e667aea274362fd922548ee3a
Frank Luongo <fluongo@oreilly.com>
2020-05-19 08:38:58 -0400

    CH5 | Controllable Pattern

--- ch5/tdd-carousel-ch5/src/tests/Carousel.test.js
+++ ch5/tdd-carousel-ch5/src/tests/Carousel.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import Carousel, { Carousel as CoreCarousel } from "../Carousel";
 import CarouselButton from "../CarouselButton";
 import CarouselSlide from "../CarouselSlide";
@@ -32,6 +32,14 @@ describe("<CarouselWithHOC />", () => {
   });
   it("passes `slides` down to the core component", () => {
     expect(wrapper.find(CoreCarousel).prop("slides")).toBe(slides);
+  });
+  it("allows `slideIndex` to be controlled", () => {
+    const mounted = mount(<Carousel slides={slides} slideIndex={1} />);
+    expect(mounted.find(CoreCarousel).props("slideIndex")).toBe(1);
+    mounted.setProps({ slideIndex: 0 });
+    expect(mounted.find(CoreCarousel))
+      .prop("slideIndex")
+      .toBe(0);
   });
 });
 
